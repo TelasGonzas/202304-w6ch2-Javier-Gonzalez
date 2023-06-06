ad0faa1e9c3426a6b91d759d6ccb414dd0c4e37a
Javier González <jgestarriol@gmail.com>
2023-06-07 00:12:51 +0200

    Add slice add test

--- src/features/phone/redux/phone.slice.test.ts
+++ src/features/phone/redux/phone.slice.test.ts
@@ -4,10 +4,12 @@ describe("phone reducer", () => {
   const initialState: PhonesState = {
     phoneNumber: "",
   };
+
   test("should handle initial state", () => {
     expect(phoneReducer(undefined, { type: "unknown" })).toEqual(initialState);
   });
-  it("should handle incrementByAmount", () => {
+
+  test("should handle incrementByAmount", () => {
     const actual = phoneReducer(initialState, add(2));
     expect(actual.phoneNumber).toEqual("2");
   });
